4c37609ee75556457b0ca5380653edcfa3d894e3
abdualla1 <38115506+abdualla1@users.noreply.github.com>
2024-12-13 22:49:09 +0200

    Update miserewithai.h
    Misere with AI and BOard DONE

--- Assignment 2/Abdalla/miserewithai.h
+++ Assignment 2/Abdalla/miserewithai.h
@@ -9,7 +9,6 @@
 #include <iomanip>
 using namespace std;
 
-// Misere Tic Tac Toe Board
 template <typename T>
 class Misere_Board : public Board<T> {
 public:
@@ -21,7 +20,6 @@ public:
     bool game_is_over() override;
 };
 
-// Misere Tic Tac Toe Player
 template <typename T>
 class Misere_Player : public Player<T> {
 public:
@@ -29,7 +27,6 @@ public:
     void getmove(int& x, int& y) override;
 };
 
-// Smart Computer Player for Misere Tic Tac Toe
 template <typename T>
 class Misere_SmartPlayer : public Player<T> {
 public:
@@ -41,9 +38,7 @@ private:
     std::pair<int, int> getBestMove();
 };
 
-//--------------------------------------- IMPLEMENTATION
 
-// Constructor for Misere_Board
 template <typename T>
 Misere_Board<T>::Misere_Board() {
     this->rows = this->columns = 3;
@@ -57,7 +52,6 @@ Misere_Board<T>::Misere_Board() {
     this->n_moves = 0;
 }
 
-// Update the board for a move
 template <typename T>
 bool Misere_Board<T>::update_board(int x, int y, T symbol) {
     if (!(x < 0 || x >= this->rows || y < 0 || y >= this->columns) && (this->board[x][y] == 0 || symbol == 0)) {
@@ -73,21 +67,23 @@ bool Misere_Board<T>::update_board(int x, int y, T symbol) {
     return false;
 }
 
-// Display the board
 template <typename T>
 void Misere_Board<T>::display_board() {
-    for (int i = 0; i < this->rows; ++i) {
-        cout << "\n| ";
-        for (int j = 0; j < this->columns; ++j) {
-            cout << "(" << i << "," << j << ")";
-            cout << setw(2) << this->board[i][j] << " |";
-        }
-        cout << "\n-----------------------------";
+    cout << "\n ";
+    for (int j = 0; j < this->columns; ++j) {
+        cout << "  " << j << "  ";
     }
     cout << endl;
+    cout << setfill('-') << setw(this->columns * 5 + 4) << "" << endl;
+    for (int i = 0; i < this->rows; ++i) {
+        cout << i << " |";
+        for (int j = 0; j < this->columns; ++j) {
+            cout << setw(3) << (this->board[i][j] == 0 ? ' ' : this->board[i][j]) << " |";
+        }
+        cout << endl << setfill('-') << setw(this->columns * 5 + 4) << "" << endl;
+    }
 }
 
-// Check if there is a losing condition
 template <typename T>
 bool Misere_Board<T>::is_win() {
     for (int i = 0; i < this->rows; ++i) {
@@ -105,30 +101,25 @@ bool Misere_Board<T>::is_win() {
     return false;
 }
 
-// Check if the game is a draw
 template <typename T>
 bool Misere_Board<T>::is_draw() {
     return (this->n_moves == 9 && !is_win());
 }
 
-// Check if the game is over
 template <typename T>
 bool Misere_Board<T>::game_is_over() {
     return is_win() || is_draw();
 }
 
-// Constructor for Misere_Player
 template <typename T>
 Misere_Player<T>::Misere_Player(string name, T symbol) : Player<T>(name, symbol) {}
 
-// Get the move from a player
 template <typename T>
 void Misere_Player<T>::getmove(int& x, int& y) {
     cout << "\nPlease enter your move x and y (0 to 2) separated by spaces: ";
     cin >> x >> y;
 }
 
-// Constructor for Misere_SmartPlayer
 template <typename T>
 Misere_SmartPlayer<T>::Misere_SmartPlayer(T symbol) : Player<T>(symbol) {
     this->name = "Player 2";
@@ -142,14 +133,13 @@ public:
 template <typename T>
 Misere_RandomPlayer<T>::Misere_RandomPlayer(T symbol) : RandomPlayer<T>(symbol) {
     this->name = "Player 2";
-    srand(static_cast<unsigned int>(time(0)));  // Seed random number generator
+    srand(static_cast<unsigned int>(time(0)));
 }
 template <typename T>
 void Misere_RandomPlayer<T>::getmove(int& x, int& y) {
         x = rand() % 3;
         y = rand() % 3;
 }
-// Get the move using MinMax
 template <typename T>
 void Misere_SmartPlayer<T>::getmove(int& x, int& y) {
     std::pair<int, int> bestMove = getBestMove();
@@ -157,15 +147,13 @@ void Misere_SmartPlayer<T>::getmove(int& x, int& y) {
     y = bestMove.second;
 }
 
-// MinMax algorithm for Misere Tic Tac Toe
-// MinMax algorithm for Misere Tic Tac Toe
 template <typename T>
 int Misere_SmartPlayer<T>::calculateMinMax(T s, bool isMaximizing) {
-    if (this->boardPtr->is_win()) {
-        return isMaximizing ? 1 : -1; // Winning is bad, so penalize the maximizer
-    } else if (this->boardPtr->is_draw()) {
-        return 0; // Neutral value for a draw
-    }
+    if (this->boardPtr->is_win())
+        return isMaximizing ? 1 : -1;
+     else if (this->boardPtr->is_draw())
+        return 0;
+
 
     int bestValue = isMaximizing ? std::numeric_limits<int>::min() : std::numeric_limits<int>::max();
     T opponentSymbol = (s == 'X') ? 'O' : 'X';
@@ -177,9 +165,9 @@ int Misere_SmartPlayer<T>::calculateMinMax(T s, bool isMaximizing) {
                 this->boardPtr->update_board(i, j, 0);
 
                 if (isMaximizing) {
-                    bestValue = std::max(bestValue, value); // Maximize losing potential
+                    bestValue = std::max(bestValue, value);
                 } else {
-                    bestValue = std::min(bestValue, value); // Minimize winning potential
+                    bestValue = std::min(bestValue, value);
                 }
             }
         }
@@ -188,7 +176,7 @@ int Misere_SmartPlayer<T>::calculateMinMax(T s, bool isMaximizing) {
     return bestValue;
 }
 
-// Get the best move using MinMax
+
 template <typename T>
 std::pair<int, int> Misere_SmartPlayer<T>::getBestMove() {
     int bestValue = std::numeric_limits<int>::min();
@@ -200,7 +188,7 @@ std::pair<int, int> Misere_SmartPlayer<T>::getBestMove() {
                 int moveValue = calculateMinMax(this->symbol, false);
                 this->boardPtr->update_board(i, j, 0);
 
-                if (moveValue > bestValue) { // Favor moves that maximize losing chances
+                if (moveValue > bestValue) {
                     bestMove = {i, j};
                     bestValue = moveValue;
                 }
