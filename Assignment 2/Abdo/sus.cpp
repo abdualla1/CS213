15d5c33c81f334fe85918bef7000039efeb5fd6a
AbdoSaber2 <161854751+AbdoSaber2@users.noreply.github.com>
2024-12-15 22:37:24 +0200

    update game 9

--- Assignment 2/Abdo/sus.cpp
+++ Assignment 2/Abdo/sus.cpp
@@ -116,28 +116,3 @@ public:
         x=rand()%3+1,y=rand()%3+1;
     }
 };
-int main() {
-    SUSBoard board;
-    cout << "Select mode: \n(1) Two Players \n(2) Player vs random player : ";
-    int chose; cin >> chose;
-    HumanPlayer player1("Player 1", 'S');
-    player1.setBoard(&board);
-    Player<char>* player2 = nullptr;
-    if(chose==1){
-        player2 = new HumanPlayer("Player 2", 'U');
-    }else{
-        player2 = new randomPlayer("Player 2", 'U');
-    }
-    Player<char>* players[2] = { &player1, player2 };
-    GameManager<char> gameManager(&board, players);
-    gameManager.run();
-    if (scores[0] > scores[1]) {
-        cout << "Player 1 wins!" << endl;
-    } else if (scores[0] < scores[1]) {
-        cout << "Player 2 wins!" << endl;
-    } else {
-        cout << "It's a draw!" << endl;
-    }
-
-    return 0;
-}
